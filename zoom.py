1e2af8586eba506fa0c815360063af7b808959ee
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-27 17:31:35 +0530

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -261,7 +261,7 @@ def sync_print(text):
 
 
 def get_driver(proxy):
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.74 Safari/537.36"
     options = webdriver.ChromeOptions()
     options.headless = True
     options.add_argument(f'user-agent={user_agent}')
