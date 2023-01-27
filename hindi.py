ede265b19508b8ce57957746f0b3d811758f659b
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-27 17:21:35 +0530

    Update hindi.py

--- hindi.py
+++ hindi.py
@@ -75,7 +75,7 @@ def get_driver(proxy):
     options.add_argument("--start-maximized")
     if proxy is not None:
         options.add_argument(f"--proxy-server={proxy}")
-    driver = webdriver.Chrome(executable_path='/snap/chromium/2295/usr/lib/chromium-browser/chrome',options=options)
+    driver = webdriver.Chrome('chrome',options=options)
     return driver
 
 
