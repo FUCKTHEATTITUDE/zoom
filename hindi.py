cd7243b866e90987376e40ff11f0b878688afcce
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-31 08:48:50 +0530

    Update hindi.py

--- hindi.py
+++ hindi.py
@@ -9,7 +9,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-option = option(executable_path=ChromeDriverManager().install())
 from selenium.webdriver.common.by import By
 
 from faker import Faker
